48c5ce7a4c531ff9951914cc75c256b8151fd792
simon <simonsingularitygroup@gmail.com>
2023-01-11 09:08:01 +0100

    add

--- lib/env.py
+++ lib/env.py
@@ -4,5 +4,5 @@ from pathlib import Path
 
 REPO_PATH = Path(__file__).resolve().parent.parent
 DATA_PATH = os.path.join(REPO_PATH, 'data')
-print("REPO_PATH=", REPO_PATH)
-print("DATA_PATH=", DATA_PATH)
+# print("REPO_PATH=", REPO_PATH)
+# print("DATA_PATH=", DATA_PATH)
